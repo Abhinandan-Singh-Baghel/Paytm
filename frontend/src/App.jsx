09a89f78bcb85a38b30f364fa75ab37827ff26ef
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-01-30 02:13:48 +0530

    Problem in routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes, } from 'react-router-dom';
+
+import { Signin } from './pages/Signin';
+import { Signup } from './pages/Signup';
+import { Dashboard } from './pages/Dashboard';
+import { SendMoney } from './pages/SendMoney';
+
+
+
+// import  Signup  from "/pages/Signup"
+// import  Signup  from "/pages/Signup"
+
+
+
+
 
 function App() {
 
@@ -8,8 +24,8 @@ function App() {
     <Routes>
     <Route path = "/signup" element={<Signup />} />
     <Route path = "/signin" element={<Signin />} />
-    <Route path = "/dashboard" element={<Dashboard />} />
-    <Route path = "/send" element={<SendMoney />} />
+    {/* <Route path = "/dashboard" element={<Dashboard />} />
+    <Route path = "/send" element={<SendMoney />} /> */}
 
     </Routes>
     
