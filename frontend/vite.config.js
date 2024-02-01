a3de01a10d07139ac343316cb6474045f5423219
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-02-01 21:53:31 +0530

    Tailwind up and running

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+// vite.config.js
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import postcss from 'vite-plugin-postcss';
+
+// // ...
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     postcss(),
+//   ],
+// });
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
