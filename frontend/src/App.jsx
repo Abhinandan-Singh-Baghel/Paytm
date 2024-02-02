1b03a39f8b83b0b8abdce06018ecb65ff3fbcaaa
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-02-02 22:09:59 +0530

    Frontend complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,9 @@ import { BrowserRouter, Route, Routes, } from 'react-router-dom';
 
 import { Signup } from './pages/Signup';
 import { Signin } from './pages/Signin';
-// import { Dashboard } from './pages/Dashboard';
-// import { SendMoney } from './pages/SendMoney';
+import { Dashboard } from './pages/Dashboard';
+import { SendMoney } from './pages/SendMoney';
 
-
-
-// import  Signup  from "/pages/Signup"
-// import  Signup  from "/pages/Signup"
 
 
 
@@ -24,8 +20,8 @@ function App() {
     <Routes>
     <Route path = "/signup" element={<Signup />} />
     <Route path = "/signin" element={<Signin />} />
-    {/* <Route path = "/dashboard" element={<Dashboard />} />
-    <Route path = "/send" element={<SendMoney />} /> */}
+    <Route path = "/dashboard" element={<Dashboard />} />
+    <Route path = "/send" element={<SendMoney />} />
 
     </Routes>
     
